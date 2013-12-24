f6cf5bda8b46ae659374f9f5057fec0c34089868
onemenny <onemenny@gmail.com>
2013-12-24 11:16:55 +0200

    adding signalR methods

--- YouTubeRemotePlayer.Web/Hubs/PlayerHub.cs
+++ YouTubeRemotePlayer.Web/Hubs/PlayerHub.cs
@@ -8,10 +8,32 @@ namespace YouTubeRemotePlayer.Web.Hubs
 {
     public class PlayerHub : Hub
     {
-        public void Send(string name, string message)
+        #region Send
+
+        public void SendPlayCommand(string videoId, string remotePlayerId)
         {
-            // Call the broadcastMessage method to update clients.
-            Clients.All.broadcastMessage(name, message);
+            //TODO send message only to client 'remotePlayerId' 
+            Clients.All.receivePlayCommand(videoId, remotePlayerId);
         }
+
+        public void SendPauseCommand(string videoId, string remotePlayerId)
+        {
+            //TODO send message only to client 'remotePlayerId' 
+            Clients.All.receivePauseCommand(videoId, remotePlayerId);
+        }
+
+        public void SendForwardCommand(string videoId, string remotePlayerId)
+        {
+            //TODO send message only to client 'remotePlayerId' 
+            Clients.All.receiveForwardCommand(videoId, remotePlayerId);
+        }
+
+        public void SendBackCommand(string videoId, string remotePlayerId)
+        {
+            //TODO send message only to client 'remotePlayerId' 
+            Clients.All.receiveBackCommand(videoId, remotePlayerId);
+        }
+
+        #endregion
     }
 }
